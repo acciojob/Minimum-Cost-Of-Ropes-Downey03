af3aa7f5905542984f9161bbb407b70d8fe751f5
Downey03 <44365074+Downey03@users.noreply.github.com>
2023-07-01 10:33:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let input = document.getElementById("rope-lengths").value;
+	  let arr = input.split(",");
+	while(arr.length>1)
+	{
+		arr.sort(function(a,b){
+			return a-b;
+		})
+		arr[1]=arr[0]+arr[1];
+		arr.shift();
+	}  
+return arr;
 }  
